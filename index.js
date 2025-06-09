d33017d0d30742d05619258a5709313a809d6bf6
Adriana Gonzalez <apgonzalez1@espe.edu.ec>
2025-06-09 14:33:44 -0500

    Subiendo proyecto myStore a GitHub

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express'); // Importar express
+const routerApi = require('./my-store/routes'); // Importar las rutas
+const { checkApiKey } = require('./my-store/middlewares/auth.handler'); // Importar middleware de autenticación
+const { logErrors, errorHandler, boomErrorHandler } = require('./my-store/middlewares/error.handler');
+const app = express(); // Asignar express a mi aplicación
+const port = 3000; // Asignación puerto donde se ejecutará el proy
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hola servidor de express');
+});
+app.get('/nueva-ruta', checkApiKey, (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+
+routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log('Mi puerto' + port);
+});
